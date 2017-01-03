a95ae68bf09398d559fbfee8c23025302c804519
AnttiHaerkonen <antth@hotmail.fi>
2017-01-03 15:01:39 +0200

    percentages

--- population.py
+++ population.py
@@ -18,7 +18,7 @@ def get_csv(file, data_dir=DATA_DIR):
 lang_groups = pd.read_csv(get_csv('lang_groups_1812_1939.csv'))
 totals = lang_groups[lang_groups.columns[1:]].sum(axis=1)
 lang_group_proportions = pd.DataFrame({'year': lang_groups['year']})
-lang_group_proportions[lang_groups.columns[1:]] = lang_groups[lang_groups.columns[1:]].div(totals, axis=0)
+lang_group_proportions[lang_groups.columns[1:]] = lang_groups[lang_groups.columns[1:]].div(totals, axis=0) * 100
 
 lang_groups.columns = ["vuosi", "suomi", "venäjä", "ruotsi", "saksa", "muut"]
 lang_group_proportions.columns = lang_groups.columns
@@ -27,13 +27,17 @@ population = pd.read_csv(get_csv('population_1799_2011.csv'))
 population.columns = ["vuosi", "väkiluku"]
 
 plt.style.use("ggplot")
-lang_groups.plot(x="vuosi", title="Viipurilaisten äidinkieli 1812-1939")
+lang_groups.plot(x="vuosi", title="Viipurin kieliryhmät 1812-1939")
 plt.show()
 
-lang_groups.plot.bar(x="vuosi", stacked=True, title="Viipurilaisten äidinkieli 1812-1939")
+lang_groups.plot.bar(x="vuosi", stacked=True, title="Viipurin kieliryhmät 1812-1939")
 plt.show()
 
-lang_group_proportions.plot.area(x="vuosi", stacked=True, title="Viipurilaisten äidinkieli 1812-1939")
+lang_group_proportions.plot.area(
+    x="vuosi",
+    stacked=True,
+    title="Viipurilaisten kieliryhmät 1812-1939\nVäestöosuudet"
+).set_ylabel('%')
 plt.show()
 
 x = 1799, 2011

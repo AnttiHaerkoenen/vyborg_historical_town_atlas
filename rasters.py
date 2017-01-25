ff91c07a0ae10e13379205956cec82bb7b1ab186
antth <antth@hotmail.fi>
2017-01-25 17:27:44 +0200

    test

--- rasters.py
+++ rasters.py
@@ -2,8 +2,12 @@ import rasterio
 
 
 def clip_histogram(file, min=9999, max=9999):
-    with rasterio.open(file) as raster:
-        pass
+    with rasterio.open(file) as file:
+        print(file.width, file.height)
+        print(file.crs)
+        print(file.transform)
+        print(file.count)
+        print(file.indexes)
 
 
 if __name__ == '__main__':

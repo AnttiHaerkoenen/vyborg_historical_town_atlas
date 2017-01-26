f0d61b0ff9dd70c94aab7754f06f46c4faa148ec
antth <antth@hotmail.fi>
2017-01-26 09:44:24 +0200

    test

--- rasters.py
+++ rasters.py
@@ -2,13 +2,13 @@ import rasterio
 
 
 def clip_histogram(file, min=9999, max=9999):
-    with rasterio.open(file) as file:
-        print(file.width, file.height)
-        print(file.crs)
-        print(file.transform)
-        print(file.count)
-        print(file.indexes)
+    with rasterio.open(file) as src:
+        print(src.width, src.height)
+        print(src.crs)
+        print(src.transform)
+        print(src.count)
+        print(src.indexes)
 
 
 if __name__ == '__main__':
-    clip_histogram()
+    clip_histogram(file='WorldDEM_Vyborg.tif')

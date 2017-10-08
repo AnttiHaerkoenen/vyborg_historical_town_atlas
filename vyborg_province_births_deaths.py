115464b970bb741babd51f9c4f07e85a5fbd9248
Antti Härkönen <antth@hotmail.fi>
2017-10-08 14:11:55 +0300

    bokeh plotting

--- vyborg_province_births_deaths.py
+++ vyborg_province_births_deaths.py
@@ -18,13 +18,14 @@ def get_csv(file, data_dir=DATA_DIR):
 
 births_deaths = pd.read_csv(get_csv('births_deaths.csv'), index_col=0)
 source = ColumnDataSource(births_deaths)
-
 f = figure(
     title="Viipurin läänin syntyneisyys, kuolleisuus ja avioituneisuus 1815-1917",
     plot_width=1000,
     plot_height=600
 )
-f.multi_line(ys=['syntyneisyys', 'kuolleisuus', 'avioituneisuus'], source=source)
+f.line(x='vuosi', y='syntyneisyys', source=source, color='green')
+f.line(x='vuosi', y='kuolleisuus', source=source, color='red')
+f.line(x='vuosi', y='avioituneisuus', source=source, color='blue')
 f.legend.location = "top_right"
 f.title.align = 'center'
 f.title.text_font_size = '20pt'
@@ -34,9 +35,7 @@ f.yaxis.major_label_text_font_size = '16pt'
 hover = HoverTool(
     tooltips=[
         ("vuosi", "@vuosi"),
-        ("syntyneisyys", "@syntyneisyys"),
-        ("kuolleisuus", "@kuolleisuus"),
-        ("avioituneisuus", "@avioituneisuus")
+        ("syntyneisyys", "@syntyneisyys")
     ],
     mode='vline'
 )

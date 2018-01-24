324d18f062a5ea2c32b978d01d067341968a7940
AnttiHaerkonen <antth@hotmail.fi>
2018-01-24 09:29:14 +0200

    default outfp

--- src/util.py
+++ src/util.py
@@ -14,7 +14,9 @@ def shp_to_geojson(input_fp: str, geojson_fp: str=None, to_epsg: int=4326) -> No
     logging.info(f"Data written to file {geojson_fp}.")
 
 
-def polygon_to_point(input_fp: str, output_fp: str) -> None:
+def polygon_to_point(input_fp: str, output_fp: str=None) -> None:
+    if not output_fp:
+        output_fp = '{0}_to_point.shp'.format(input_fp.split('.')[0])
     data_ = gpd.read_file(input_fp)
     data_.to_file(output_fp)
     logging.info(f"Data written to file {output_fp}.")

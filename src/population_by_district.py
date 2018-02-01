18210d5c0385cb346315d92aa9ebf288ea1d123e
AnttiHaerkonen <antth@hotmail.fi>
2018-02-01 16:04:46 +0200

    combine data

--- src/population_by_district.py
+++ src/population_by_district.py
@@ -1,8 +1,21 @@
 import os
+import logging
 
 import geopandas as gpd
 import pandas as pd
 
+from src.util import combine_data
+
 
 if __name__ == '__main__':
-    pass
+    logging.basicConfig(level=logging.INFO)
+    os.chdir('..\data')
+    data_ = combine_data(
+        'districts_1928.shp',
+        'population_1870_1890.xlsx',
+        sheet='1870',
+        shp_on='NAME',
+        stats_on='Kaupunginosa',
+        how='inner'
+    )
+    print(data_)
--- src/util.py
+++ src/util.py
@@ -23,7 +23,7 @@ def polygon_to_point(
     logging.info(f"polygon_to_point: Data written to file {output_fp}.")
 
 
-def combine_data(
+def combine_data_to_file(
         shp_fp: str,
         stats_fp: str,
         output_fp: str=None,
@@ -51,6 +51,54 @@ def combine_data(
     data_ = data_.join(data_stats, **kwargs)
     data_.to_file(output_fp, crs_wkt=crs_, encoding='utf-8')
     logging.info(f"combine_data: Data written to file {output_fp}.")
+
+
+def combine_data(
+        shp_fp: str,
+        stats_fp: str,
+        shp_on: str=None,
+        stats_on: str=None,
+        sheet: str or int=None,
+        **kwargs
+) -> gpd.GeoDataFrame or None:
+    """
+    Combines spatial and non-spatial data from files using pandas.DataFrame and geopandas.GeoDataFrame.
+    :param shp_fp: Filepath to spatial data (shapefile)
+    :param stats_fp: Filepath to non-spatial data (csv, xls, xlsx)
+    :param shp_on: Which column of spatial data to use in join
+    :param stats_on: Which column of non-spatial data to use in join
+    :param sheet: which excel sheet to use
+    :param kwargs: Additional arguments for pandas.DataFrame.join
+    :return: geopandas.GeoDataFrame with joined data
+    """
+    data_ = gpd.read_file(shp_fp)
+    with open(shp_fp.replace('.shp', '.prj')) as crs_fin:
+        crs_ = crs_fin.readline().strip()
+
+    stats_format = stats_fp.split('.', maxsplit=1)[1]
+    if stats_format == 'csv':
+        data_stats = pd.read_csv(stats_fp)
+    elif stats_format in ('xls', 'xlsx'):
+        data_stats = pd.read_excel(stats_fp, sheet=sheet)
+    else:
+        logging.error(f'combine_data: {stats_format} is not supported data format')
+        return None
+
+    if shp_on:
+        try:
+            data_ = data_.set_index(shp_on)
+        except KeyError:
+            logging.error(f"{shp_on} is wrong key. Check spelling")
+            return None
+    if stats_on:
+        try:
+            data_stats = data_stats.set_index(stats_on)
+        except KeyError:
+            logging.error(f"{stats_on} is wrong key. Check spelling")
+            return None
+
+    data_ = data_.join(data_stats, **kwargs)
+    return data_
 
 
 def shp_to_geojson(
@@ -105,14 +153,14 @@ def remove_umlauts(text):
 
 if __name__ == '__main__':
     logging.basicConfig(level=logging.INFO)
-    os.chdir(r'..\data')
+    os.chdir('..\data')
 
     polygon_to_point('districts_1928.shp')
-    combine_data(
+    combine_data_to_file(
         'districts_1928_to_point.shp',
         'population_1870_1890.xlsx',
         stats_on='Kaupunginosa',
-        on='NAME',
+        shp_on='NAME',
         how='inner'
     )
     shp_to_geojson('districts_1928_to_point_population_1870_1890.shp')

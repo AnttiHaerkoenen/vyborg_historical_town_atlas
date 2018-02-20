aae3260347c00eeeea9c6997c21b158c837b1de3
AnttiHaerkoenen <antth@hotmail.fi>
2018-02-20 18:24:56 +0200

    rtd style

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -184,7 +184,6 @@ texinfo_documents = [
 ]
 
 
-
 # -- Options for Epub output ----------------------------------------------
 
 # Bibliographic Dublin Core info.
@@ -204,7 +203,6 @@ epub_copyright = copyright
 
 # A list of files that should not be packed into the epub file.
 epub_exclude_files = ['search.html']
-
 
 
 # Example configuration for intersphinx: refer to the Python standard library.

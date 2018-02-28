f1a35eeac08ccbfe81bf4f41913eadff53cf0255
AnttiHaerkonen <antth@hotmail.fi>
2018-02-28 17:50:45 +0200

    plotting

--- src/karonen.py
+++ src/karonen.py
@@ -1,7 +1,5 @@
 import os
-import logging
-from datetime import datetime
-import time
+
 
 import squarify
 import geopandas as gpd
@@ -9,21 +7,18 @@ import pandas as pd
 import numpy as np
 from bokeh.plotting import figure, show, output_file
 from bokeh.layouts import gridplot
-from bokeh.palettes import Category10_10 as palette
+from bokeh.palettes import Category10_10
 from bokeh.models import (
     HoverTool,
     GeoJSONDataSource,
     Title,
     BasicTicker
 )
-from bokeh.client import pull_session
-from bokeh.embed import server_session
-from flask import Flask, render_template
 
 from src.util import *
 
 
-def get_mosaic(data_: pd.DataFrame, *, data_col: str, x=0, y=0, width, height) -> dict:
+def get_mosaic(data_: pd.DataFrame, *, data_col: str, x=0.0, y=0.0, width, height) -> dict:
     data = data_.sort_values(by=data_col, ascending=False)
     data = data.loc[data[data_col] > 0]
     rectangles = squarify.normalize_sizes(list(data[data_col]), width, height)
@@ -54,21 +49,55 @@ def get_mosaic(data_: pd.DataFrame, *, data_col: str, x=0, y=0, width, height) -
 if __name__ == '__main__':
     os.chdir(r'../data')
     data_ = pd.read_csv('population_1570.csv', index_col=0)
-    data_['colors'] = palette[:len(data_)]
+    palette = Category10_10[:len(data_)]
+    data_['colors'] = palette
+    groups = [g for g in data_.index]
+    water = gpd.read_file('water_clip.shp')
+
+    water = get_xy(multipolygons_to_polygons(water))
+    water_src = GeoJSONDataSource(geojson=water.to_json())
+
+    islands = gpd.read_file('islands.shp')
+    islands = get_xy(multipolygons_to_polygons(islands))
+    islands_src = GeoJSONDataSource(geojson=islands.to_json())
 
     fig = figure(
-        plot_width=600,
-        plot_height=600,
+        plot_height=750,
+        plot_width=700,
+        x_axis_location=None,
+        y_axis_location=None,
+        y_range=(60.7, 60.72),
+        x_range=(28.72, 28.745),
     )
-    mosaic_data = get_mosaic(data_, data_col='iii', x=0, y=0, height=100, width=100)
+
+    fig.grid.grid_line_color = None
+
+    fig.patches(
+        xs='x',
+        ys='y',
+        source=water_src,
+        fill_color='#59d0ff',
+        fill_alpha=0.8,
+        line_color=None,
+        line_width=0,
+    )
+    fig.patches(
+        xs='x',
+        ys='y',
+        source=islands_src,
+        fill_color='white',
+        line_color=None,
+        line_width=0
+    )
+
+    mosaic_data = get_mosaic(data_, data_col='i', x=28.729, y=60.712, height=0.0011, width=0.002)
     fig.patches(**mosaic_data)
-    mosaic_data = get_mosaic(data_, data_col='ii', x=120, y=120, height=100, width=100)
-    fig.patches(**mosaic_data)
-    mosaic_data = get_mosaic(data_, data_col='iv', x=120, y=0, height=100, width=100)
-    fig.patches(**mosaic_data)
-    mosaic_data = get_mosaic(data_, data_col='i', x=0, y=120, height=100, width=100)
-    fig.patches(**mosaic_data)
-    print(data_)
+
+    for group, color in zip(groups, palette):
+        fig.circle(x=[], y=[], fill_color=color, legend=group)
+    fig.legend.location = 'bottom_left'
+    fig.legend.background_fill_alpha = 1
+    fig.legend.border_line_color = 'black'
 
     output_file(r'../figures/karonen.html')
 

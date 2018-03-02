d9d8d0311e0a473afa514297b5cb774f5da27859
AnttiHaerkonen <antth@hotmail.fi>
2018-03-02 16:06:06 +0200

    stacked bar plots

--- src/karonen.py
+++ src/karonen.py
@@ -48,6 +48,36 @@ def get_mosaic(data_: pd.DataFrame, *, data_col: str, x=0.0, y=0.0, width, heigh
     }
 
 
+def get_stacked_bar(data: pd.DataFrame, *, data_col: str, x=0.0, y=0.0, width, height) -> dict:
+    data = data.loc[data[data_col] > 0][::-1]
+    lens = data[data_col] / data[data_col].sum() * height
+    xs, ys = [], []
+    last_y = y
+    for l in lens:
+        xs.append([x, x + width, x + width, x])
+        y_ = [
+            last_y + l,
+            last_y + l,
+            last_y,
+            last_y,
+        ]
+        last_y += l
+        ys.append(y_)
+
+    colors = [c for c in data['colors']]
+
+    return {
+        'xs': xs,
+        'ys': ys,
+        'color': colors,
+    }
+
+
+def get_stacked_bars(data: pd.DataFrame, *, data_col: str, group_col: str, x=0.0, y=0.0, width, height) -> dict:
+    for group in set(data[group_col]):
+        group_data = data.loc[data[group_col] == group]
+
+
 def draw_population_map(
         *,
         population_file,
@@ -55,9 +85,14 @@ def draw_population_map(
         islands_file,
         districts_file,
         locations,
-        size,
+        width,
+        height=None,
+        type,
         **kwargs
 ):
+    _types = 'mosaic', 'stacked bar'
+    if type not in _types:
+        raise ValueError(f'Incorrect type parameter (Must be one of {_types})')
     data_ = pd.read_csv(population_file, index_col=0)
     totals = data_.sum(axis=0)
     palette = Category10_10[:len(data_)]
@@ -106,17 +141,31 @@ def draw_population_map(
         line_width=0
     )
 
-    for col, loc in locations.items():
-        width = size * np.sqrt(totals[col] / totals.max())
-        mosaic_data = get_mosaic(
-            data_,
-            data_col=col,
-            x=loc.x,
-            y=loc.y,
-            height=width * np.cos(loc.y * 0.01745),
-            width=width,
-        )
-        fig.patches(**mosaic_data)
+    if type.lower() == 'mosaic':
+        for col, loc in locations.items():
+            width = width * np.sqrt(totals[col] / totals.max())
+            mosaic_data = get_mosaic(
+                data_,
+                data_col=col,
+                x=loc.x,
+                y=loc.y,
+                height=width * np.cos(loc.y * 0.01745),
+                width=width,
+            )
+            fig.patches(**mosaic_data)
+
+    if type.lower() == 'stacked bar':
+        for col, loc in locations.items():
+            height = height * totals[col] / totals.max()
+            stack_data = get_stacked_bar(
+                data_,
+                data_col=col,
+                x=loc.x,
+                y=loc.y,
+                height=height,
+                width=width,
+            )
+            fig.patches(**stack_data)
 
     for group, color in zip(groups, palette):
         fig.circle(x=[], y=[], fill_color=color, legend=group)
@@ -128,46 +177,110 @@ def draw_population_map(
 
 if __name__ == '__main__':
     os.chdir(r'../data')
-    fig1 = draw_population_map(
-        population_file='population_1570.csv',
+    kwargs = dict(
         water_file='water_1698.shp',
         islands_file='islands_1698.shp',
-        districts_file='districts_1637.shp',
-        plot_height=900,
-        plot_width=1000,
+        plot_height=2000,
+        plot_width=2000,
         x_axis_location=None,
         y_axis_location=None,
+    )
+    fig1 = draw_population_map(
+        population_file='population_1570.csv',
+        districts_file='districts_1637.shp',
         y_range=(60.705, 60.717),
         x_range=(28.722, 28.743),
-        size=0.0018,
+        width=0.002,
+        type='mosaic',
         locations={
             'i': Coordinates(28.73, 60.7125),
             'ii': Coordinates(28.732, 60.7105),
             'iii': Coordinates(28.7315, 60.7137),
             'iv': Coordinates(28.734, 60.713),
-            'Valli': Coordinates(28.738, 60.7105),
+            'Valli': Coordinates(28.737, 60.7105),
         },
+        **kwargs
     )
     fig2 = draw_population_map(
         population_file='population_1630.csv',
-        water_file='water_1698.shp',
-        islands_file='islands_1698.shp',
         districts_file='districts_1703.shp',
-        plot_height=900,
-        plot_width=1000,
-        x_axis_location=None,
-        y_axis_location=None,
         y_range=(60.705, 60.718),
         x_range=(28.72, 28.743),
-        size=0.0025,
+        width=0.004,
+        type='mosaic',
         locations={
             'Linnoitus': Coordinates(28.732, 60.7125),
-            'Siikaniemi': Coordinates(28.7235, 60.7125),
-            'Valli': Coordinates(28.738, 60.7105),
+            'Siikaniemi': Coordinates(28.723, 60.7125),
+            'Valli': Coordinates(28.737, 60.7102),
             'Pantsarlahti': Coordinates(28.738, 60.7057),
         },
+        **kwargs
+    )
+    fig3 = draw_population_map(
+        population_file='population_1700.csv',
+        districts_file='districts_1703.shp',
+        y_range=(60.705, 60.718),
+        x_range=(28.72, 28.743),
+        width=0.004,
+        type='mosaic',
+        locations={
+            'Linnoitus': Coordinates(28.732, 60.7125),
+            'Siikaniemi': Coordinates(28.723, 60.7125),
+            'Valli': Coordinates(28.737, 60.7102),
+            'Pantsarlahti': Coordinates(28.738, 60.7059),
+        },
+        **kwargs
+    )
+    fig4 = draw_population_map(
+        population_file='population_1570.csv',
+        districts_file='districts_1637.shp',
+        y_range=(60.705, 60.717),
+        x_range=(28.722, 28.743),
+        width=0.0008,
+        height=0.0015,
+        type='stacked bar',
+        locations={
+            'i': Coordinates(28.73, 60.7125),
+            'ii': Coordinates(28.732, 60.7105),
+            'iii': Coordinates(28.7315, 60.7137),
+            'iv': Coordinates(28.734, 60.713),
+            'Valli': Coordinates(28.737, 60.7105),
+        },
+        **kwargs
+    )
+    fig5 = draw_population_map(
+        population_file='population_1630.csv',
+        districts_file='districts_1703.shp',
+        y_range=(60.705, 60.718),
+        x_range=(28.72, 28.743),
+        width=0.0008,
+        height=0.004,
+        type='stacked bar',
+        locations={
+            'Linnoitus': Coordinates(28.732, 60.712),
+            'Siikaniemi': Coordinates(28.724, 60.7122),
+            'Valli': Coordinates(28.737, 60.710),
+            'Pantsarlahti': Coordinates(28.738, 60.7057),
+        },
+        **kwargs
+    )
+    fig6 = draw_population_map(
+        population_file='population_1700.csv',
+        districts_file='districts_1703.shp',
+        y_range=(60.705, 60.718),
+        x_range=(28.72, 28.743),
+        width=0.0008,
+        height=0.004,
+        type='stacked bar',
+        locations={
+            'Linnoitus': Coordinates(28.732, 60.712),
+            'Siikaniemi': Coordinates(28.724, 60.7122),
+            'Valli': Coordinates(28.737, 60.710),
+            'Pantsarlahti': Coordinates(28.738, 60.7059),
+        },
+        **kwargs
     )
 
     output_file(r'../figures/karonen.html')
-
-    show(fig2)
+    show(gridplot([fig1, fig2, fig3], ncols=1))
+    show(gridplot([fig4, fig5, fig6], ncols=1))
